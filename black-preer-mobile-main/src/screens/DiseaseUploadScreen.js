37daf06e4993b92247d65f38407d5209f252fd9e
DinuniWanniarachchi <it22293176@my.sliit.lk>
2026-05-01 12:44:13 +0530

    fix: update API URLs to correct server address

--- black-preer-mobile-main/src/screens/DiseaseUploadScreen.js
+++ black-preer-mobile-main/src/screens/DiseaseUploadScreen.js
@@ -20,10 +20,7 @@ const { width } = Dimensions.get('window');
 const isSmallScreen = width < 480;
 const isWideScreen = width >= 768;
 
-const API_BASE_URL =
-  Platform.OS === 'web'
-    ? 'http://localhost:5001'
-    : 'http://192.168.8.110:5001';
+const API_BASE_URL = 'http://10.137.134.38:5000';
 
 export default function DiseaseUploadScreen({ navigation }) {
   const [selectedAsset, setSelectedAsset] = useState(null);
